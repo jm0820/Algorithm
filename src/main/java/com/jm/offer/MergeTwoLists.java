304a2af5945236ddb1944c7603926c51acc26108
jm0820 <18717545422@163.com>
2021-09-30 19:50:20 +0800

    flash

--- src/main/java/com/jm/offer/MergeTwoLists.java
+++ src/main/java/com/jm/offer/MergeTwoLists.java
@@ -1,2 +1,50 @@
-package com.jm.offer;public class MergeTwoLists {
+package com.jm.offer;
+
+/**
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ *
+ * 示例1：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ */
+public class MergeTwoLists {
+    public static void main(String[] args) {
+
+    }
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head,p;
+        if (l1 == null){
+            return l2;
+        }
+        if (l2 == null){
+            return l1;
+        }
+        //先确定头节点
+        if(l1.val > l2.val){
+            head = l2;
+            l2 = l2.next;
+        }else {
+            head = l1;
+            l1 = l1.next;
+        }
+        p = head;
+        while (l1 !=null && l2 != null){
+            if(l1.val > l2.val){
+                p.next = l2;
+                l2 = l2.next;
+            }else {
+                p.next = l1;
+                l1 = l1.next;
+            }
+            p = p.next;
+        }
+        if(l1 != null){
+            p.next = l1;
+        }else {
+            p.next = l2;
+        }
+        return head;
+    }
 }
--- src/main/java/com/jm/offer/Symmetric.java
+++ src/main/java/com/jm/offer/Symmetric.java
@@ -1,2 +1,18 @@
-package com.jm.offer;public class Symmetric {
+package com.jm.offer;
+
+/**
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的
+ */
+public class Symmetric {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isSymmetric(TreeNode root) {
+        return root == null ? true : recur(root.left, root.right);
+    }
+    public static boolean recur(TreeNode L, TreeNode R) {
+        if(L == null && R == null) return true;
+        if(L == null || R == null || L.val != R.val) return false;
+        return recur(L.left, R.right) && recur(L.right, R.left);
+    }
 }
